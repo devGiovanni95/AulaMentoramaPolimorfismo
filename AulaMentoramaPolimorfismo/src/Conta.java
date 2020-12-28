b888faf4b7d0c9dc80f87f232a93c77aaada3910
Giovanni Almeida dos Santos <giovanni.DevJava@hotmail.com>
2020-12-27 23:14:20 -0300

    Sistema de banco

--- AulaMentoramaPolimorfismo/src/Conta.java
+++ AulaMentoramaPolimorfismo/src/Conta.java
@@ -4,75 +4,14 @@ public abstract class Conta {
     private int agencia;
     private String banco;
     protected double saldo;
-    protected double sacar;
-    protected double depositar;
-    protected double saldoAtualizado;
-    protected double saldoAtualizado2;
-
-
-    public int getNumero() {
-        return numero;
-    }
-
-    public void setNumero(int numero) {
-        this.numero = numero;
-    }
-
-    public int getAgencia() {
-        return agencia;
-    }
-
-    public void setAgencia(int agencia) {
-        this.agencia = agencia;
-    }
-
-    public String getBanco() {
-        return banco;
-    }
-
-    public void setBanco(String banco) {
-        this.banco = banco;
-    }
 
     public abstract double getSaldo();
 
-
-    public void setSaldo(double saldo) {
-        this.saldo = saldo;
-    }
-
-    public abstract double getSacar();
-
-    public void setSacar(double sacar) {
-        this.sacar = sacar;
-    }
-
-    public abstract double getDepositar();
-
-    public void setDepositar(double depositar) {
-        this.depositar = depositar;
-    }
-
-    public abstract double getSaldoAtualizado();
-
-    public abstract double getSaldoAtualizado2();
-
-
-    public void setSaldoAtualizado(double saldoAtualizado) {
-        this.saldoAtualizado = saldoAtualizado;
-    }
-
-    public void setSaldoAtualizado2(double saldoAtualizado2) {
-        this.saldoAtualizado2 = saldoAtualizado2;
-    }
-
-    public Conta(int numero, int agencia, String banco, double saldo, double sacar, double depositar) {
+    public Conta(int numero, int agencia, String banco, double saldo) {
         this.numero = numero;
         this.agencia = agencia;
         this.banco = banco;
         this.saldo = saldo;
-        this.sacar = sacar;
-        this.depositar = depositar;
     }
 
     @Override
@@ -82,8 +21,10 @@ public abstract class Conta {
                 ", agencia=" + agencia +
                 ", banco='" + banco + '\'' +
                 ", saldo=" + saldo +
-                ", sacar=" + sacar +
-                ", depositar=" + depositar +
                 '}';
     }
+
+    public abstract boolean sacar(double quantia);
+
+    public void depositar(double quantia) { this.saldo += quantia;}
 }
--- AulaMentoramaPolimorfismo/src/ContaPoupanca.java
+++ AulaMentoramaPolimorfismo/src/ContaPoupanca.java
@@ -1,2 +1,23 @@
-public class ContaPoupanca {
+public class ContaPoupanca extends Conta{
+    private int diaAniversario;
+    private double taxaDeJuros;
+
+    public ContaPoupanca(int numero, int agencia, String banco, double saldo, int diaAniversario, double taxaDeJuros) {
+        super(numero, agencia, banco, saldo);
+        this.diaAniversario = diaAniversario;
+        this.taxaDeJuros = taxaDeJuros;
+    }
+
+    @Override
+    public double getSaldo() {
+        return this.saldo + this.taxaDeJuros*this.saldo;
+    }
+
+    @Override
+    public boolean sacar(double quantia) {
+        this.saldo -= quantia;
+        return  true;
+    }
 }
+
+
